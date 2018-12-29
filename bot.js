be00c3495c1caf2591847b8fd3f4c5a8a348c27b
belal9999 <45916807+belal9999@users.noreply.github.com>
2018-12-29 16:26:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
 });
 
 
-const developers = ["424134723716120578"]
+const developers = ["479008572722905123"]
 const adminprefix = "$";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
