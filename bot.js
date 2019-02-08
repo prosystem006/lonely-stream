bf7fb485909bd5f8e91d9b87e31d2813d5eff6ce
prosystem006 <46976423+prosystem006@users.noreply.github.com>
2019-02-08 19:34:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
 });
 
 
-const developers = ["479008572722905123"]
+const developers = ["320314934858874880"]
 const adminprefix = "$";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
